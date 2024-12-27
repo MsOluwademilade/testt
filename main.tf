af6baa12123ab55f46c034adb977ed6db8a13c5d
MsOluwademilade <oyekanmidemilade2@gmail.com>
2024-12-27 12:05:59 +0000

    Fixed tf file

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
 
   subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_securiy_group.instance.id]
+  vpc_security_group_ids      = [aws_security_group.instance.id]
   associate_public_ip_address = true
   key_name                   = aws_key_pair.deployer.key_name
 
